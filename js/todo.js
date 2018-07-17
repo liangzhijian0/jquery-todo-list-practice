78bf653086e8b632f00d608e832a0fb21997591e
Ocean Liang <657175454@qq.com>
2018-07-18 00:58:05 +0800

    refactor todoForm.todos.find(element => element.id === viewId).name = $(event.target).text();

--- js/todo.js
+++ js/todo.js
@@ -1,148 +1,100 @@
 $(document)
     .ready(function () {
 
-        function generateUUID() {
-            /*jshint bitwise:false */
-            var i,
-                random;
-            var uuid = '';
+    function generateUUID() {
+        /*jshint bitwise:false */
+        var i,
+            random;
+        var uuid = '';
 
-            for (i = 0; i < 32; i++) {
-                random = Math.random() * 16 | 0;
-                if (i === 8 || i === 12 || i === 16 || i === 20) {
-                    uuid += '-';
-                }
-                uuid += (i === 12
-                    ? 4
-                    : (i === 16
-                        ? (random & 3 | 8)
-                        : random)).toString(16);
+        for (i = 0; i < 32; i++) {
+            random = Math.random() * 16 | 0;
+            if (i === 8 || i === 12 || i === 16 || i === 20) {
+                uuid += '-';
             }
-            return uuid;
+            uuid += (i === 12
+                ? 4
+                : (i === 16
+                    ? (random & 3 | 8)
+                    : random)).toString(16);
         }
-        var todoList = [
-            {
-                complete:false,
-                list: 'Parking Lot APP Refactor'   
-            },
-            {
-                complete:true,
-                list: 'Parking Lot APP 时序图' 
-            },
-            {
-                complete:false,
-                list: 'Parking Lot APP Refactor'   
-            },
-            {
-                complete:true,
-                list: 'Parking Lot APP 时序图' 
-            },
-            {
-                complete:false,
-                list: 'Parking Lot APP Refactor'   
-            },
-            {
-                complete:true,
-                list: 'Parking Lot APP 时序图' 
-            },
-        ]
-        // function initOriginal(){
-        //     $("#filters").find("li").eq(0).children("a").removeClass("selected");
-        //     $("#filters").find("li").eq(1).children("a").removeClass("selected");
-        //     $("#filters").find("li").eq(2).children("a").removeClass("selected");
-        //     $("#list-content").find("li").show();
-        // }
-
-        // // code to be implemented
-        // $(document).delegate(".done-todo", "click", function() {
-        //    $(this).parent().toggleClass("checked");
-        // })
-
-        // $("#button").click(function(){
-        //     value = $(".input-text").val();
-        //     if(value === ''){
-        //         alert('Can not empty!');
-        //         return;
-        //     }
-        //     $("#list-content").append(" <li id='" +generateUUID() +"' class=''>"+        
-        //     "<input name='done-todo' type='checkbox' class='done-todo'>"+ value +"</li>") 
-        //     $(".input-text").val('');
-        //  })
-
-
-        // $("#filters").find("li").eq(0).click(function(){
-        //     initOriginal();
-        //     $(this).children("a").addClass("selected");         
-        // })
-
-        // $("#filters").find("li").eq(1).click(function(){
-        //     initOriginal();
-        //     $(this).children("a").addClass("selected");        
-        //     $("#list-content .checked").hide();
-        // })
-
-        // $("#filters").find("li").eq(2).click(function(){
-        //     initOriginal();
-        //     $(this).children("a").addClass("selected");
-        //     $("#list-content li[class!='checked']").hide();
-        // })
-
-        // $('#list-content').find("li").click(function(){      
-        //     $(this).attr("contentEditable",'true')
-        //     $(this).children().attr("contentEditable",'false')
-        // });
-
-    let template=`
-        <div>
-            <input class="input-text" type="text" name="ListItem" data-com.agilebits.onepassword.user-edited="yes">
-            <div id="button" >Add</div>
-        </div>
-        <br>
-        <ol id="list-content">
-            ${buildTodoList()}
-        </ol>
-        <div>
-            <ul id="filters">
-                <li>
-                    <a href="#" data-filter="all" class="selected" >ALL</a>
-                </li>
-                <li>
-                    <a href="#" data-filter="active" class="">Active</a>
-                </li>
-                <li>
-                    <a href="#" data-filter="complete" class="">Complete</a>
-                </li>
-            </ul>
-        </div>`
-
-
-    
-
-    // const render = () =>{
-    //     $('#todoForm').html(template);
-    // }
-    function render(){
-        $('#todoForm').html(bulidHTML());
+        return uuid;
     }
+
+    let todoList = {
+        status:'all',
+        todos:
+            [{
+                id:'111232',
+                name: 'Parking Lot APP Refactor',
+                isComplete:false
+            },
+            {
+                id:'113232',
+                name: 'JQQQQQQQ',
+                isComplete:true
+            }]      
+    }
+
+    function filterTodos(todos,status){
+        const filterExecuters = {
+            all() {
+                return true;
+            },
+            active(item) {
+                return !item.isComplete; 
+            },
+            complete(item) {
+                return item.isComplete;
+            }
+        }
+        return todos.filter(filterExecuters[status]); 
+    }
+
+    const render = () =>{
+        $('#todoForm').html(bulidHTML(todoList));
+    }
+    
     render();
 
     window.addItem = function(){
         var toAdd = $('input[name=ListItem]').val();
-        todoList.push({   complete:false,list:toAdd});
-        render();
-        $('input[name=ListItem]').val("");
-    }
-
-    function buildTodoList(){
-        str ='';
-        for(let i=0;i<todoList.length;i++){
-            str += `<li id="${generateUUID()}" class="${todoList[i].complete ? 'checked':''}">
-            <input name="done-todo" type="checkbox" class="done-todo"> ${todoList[i].list} </li>`;
+        if(toAdd === ''){
+            alert('Can not empty')
+            return;
         }
-        return str;
+        todoList.todos.push({id:generateUUID(),name:toAdd,isComplete:false});
+        render();
+        
     }
 
-    function bulidHTML(){
+    window.changeCheck = function(eventId){
+        let target = todoList.todos.find(item => item.id === eventId);
+        target.isComplete = !target.isComplete;
+        render();
+    }
+
+    window.showStatus = (status) => {
+        todoList.status = status;
+        render();
+    }
+
+    window.editContent = (event,eventId) => {
+        $(event.target).attr('contentEditable', 'true')
+        .focus()
+        .keypress(e => {
+            if(e.keyCode == '13'){
+                todoList.todos.find(element => element.id === eventId).name = $(event.target).text();
+                render();
+            }
+        })
+    }
+
+    function bulidHTML(todoList){
+        let buildTodoList = (item) => 
+            `<li class="${item.isComplete ? 'checked':''}" ondblclick="editContent(event,'${item.id}')">
+            <input name="done-todo" type="checkbox" class="done-todo" ${item.isComplete ? 'checked':''} onclick="changeCheck('${item.id}')"> ${item.name} </li>`;
+
         let template=`
         <div>
             <input class="input-text" type="text" name="ListItem" data-com.agilebits.onepassword.user-edited="yes">
@@ -150,23 +102,21 @@ $(document)
         </div>
         <br>
         <ol id="list-content">
-            ${buildTodoList()}
+            ${filterTodos(todoList.todos,todoList.status).map(buildTodoList).join("")}
         </ol>
         <div>
-            <ul id="filters">
-                <li onclick="showAll()">
-                    <a href="#" data-filter="all" class="selected" >ALL</a>
+            <ul id="filters" >
+                <li onclick="showStatus('all')">
+                    <a href="#" data-filter="all" class="${todoList.status === 'all' ? 'selected':''}" >ALL</a>
                 </li>
-                <li onclick="showActive()">
-                    <a href="#" data-filter="active" class="">Active</a>
+                <li onclick="showStatus('active')">
+                    <a href="#" data-filter="active" class="${todoList.status === 'active' ? 'selected':''}">Active</a>
                 </li>
-                <li onclick="showComplete()">
-                    <a href="#" data-filter="complete" class="">Complete</a>
+                <li onclick="showStatus('complete')">
+                    <a href="#" data-filter="complete" class="${todoList.status === 'complete' ? 'selected':''}">Complete</a>
                 </li>
             </ul>
         </div>`
         return template;
     }
-
-
 });
